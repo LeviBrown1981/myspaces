f7f5d397e7e909b3d57e22a5e81d5a6d88333356
Levi Brown <rlevibrown@gmail.com>
2019-09-24 14:21:58 -0600

    added Home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,79 @@
 import React from 'react';
-import { Header, } from 'semantic-ui-react';
+import axios from 'axios';
+import { Link, } from 'react-router-dom';
+import { Header, Image, Card, Button, Icon, } from 'semantic-ui-react';
 
-const Home = () => (
-  <Header as="h3" textAlign="center">Myspace</Header>
-)
+class Home extends React.Component {
+  state = { people: [], };
+  
+  componentDidMount() {
+    axios.get('/api/people')
+      .then(res => this.setState({ people: res.data, }))
+  }
+  
+  downVote = (id) => {
+    const { cats, } = this.state;
+    this.setState({ cats: cats.filter( c => c.id !== id ), });
+  }
+
+  upvote = (id) => {
+    const { cats, } = this.state;
+    axios.put(`/api/cats/${id}`)
+      .then( () => this.setState({ cats: cats.filter( c => c.id !== id ), }) )
+  }
+
+  sample = () => {
+    const { people, } = this.state;
+
+    if (people.length) {
+      const index = Math.floor(Math.random() * people.length);
+      return people[index];
+    } else {
+      return null;
+    }
+  }
+  
+  render() {
+    const person = this.sample();
+    if (person) {
+      return (
+        <div>
+          <br />
+          <Header as='h1'>person Tinder</Header>
+          <br />
+          <Card key={person.id}>
+            <Image src={person.avatar} />
+            <Card.Content>
+              <Card.Header>
+                { person.name }
+              </Card.Header>
+              <Card.Description>
+                { person.breed }
+              </Card.Description>
+              <Card.Meta>
+                { person.registry }
+              </Card.Meta>
+            </Card.Content>
+            <Card.Content extra>
+            <Button color="red" icon basic onClick={() => this.downVote(cat.id)}>
+              <Icon name="thumbs down" />
+            </Button>
+            <Button color="green" icon basic onClick={() => this.upvote(cat.id)}>
+              <Icon name="thumbs up" />
+            </Button>
+            </Card.Content>
+          </Card>
+          <Link to="/my_people">
+            <Button color="blue">
+              My People
+            </Button>
+          </Link>
+        </div>
+      );
+    } else {
+      return <Header textAlign="center">No More People</Header>
+    }
+  }
+}
 
 export default Home;
